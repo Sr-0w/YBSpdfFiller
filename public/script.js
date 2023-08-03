73214926250cd4601bf90e42db99b0c90b93cd2d
Sr-0w <139899120+Sr-0w@users.noreply.github.com>
2023-08-03 18:39:41 +0200

    initiating download instead of redirecting

--- public/script.js
+++ public/script.js
@@ -9,7 +9,7 @@ document.getElementById('securitasForm').addEventListener('submit', function(eve
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify(formData)
+        body: JSON.stringify(Object.fromEntries(formData)) // Convert formData to JSON
       })
       .then(response => {
         if (!response.ok) {
@@ -19,7 +19,20 @@ document.getElementById('securitasForm').addEventListener('submit', function(eve
       })
       .then(blob => {
         const url = URL.createObjectURL(blob); // Create a URL for the Blob
-        window.open(url, '_blank'); // Open the URL in a new tab or window
+
+        // Create a link element
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'filledPdf.pdf'; // Set the download filename
+
+        // Append the link to the body
+        document.body.appendChild(link);
+
+        // Simulate a click on the link
+        link.click();
+
+        // Remove the link from the body
+        document.body.removeChild(link);
       })
       .catch(error => {
         console.error('Error:', error);
