87406e81670f67c2a7e93426afd2ca482531b931
Sr-0w <139899120+Sr-0w@users.noreply.github.com>
2023-08-03 18:24:43 +0200

    Try with hummus

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
-const pdfFillForm = require('pdf-fill-form');
-const path = require('path'); // Add this line
+const path = require('path');
+const HummusRecipe = require('hummus-recipe'); // Add this line
 
 const app = express();
 app.use(bodyParser.json());
@@ -20,19 +20,29 @@ app.post('/submit', async (req, res) => {
     const formData = req.body;
     console.log('Form data:', formData);
 
-    // Read the PDF file
-    console.log('Reading PDF file');
-    const pdfPath = path.join(__dirname, 'public', 'securitashomeyoul.pdf'); // Add this line
-    const pdfBuffer = fs.readFileSync(pdfPath); // Modify this line
+    // Define the PDF file paths
+    const pdfPath = path.join(__dirname, 'public', 'securitashomeyoul.pdf');
+    const outputPdfPath = path.join(__dirname, 'public', 'filledPdf.pdf');
+
+    // Create a new HummusRecipe instance
+    const pdfDoc = new HummusRecipe(pdfPath, outputPdfPath);
 
     // Fill in the fields in the PDF
     console.log('Filling PDF');
-    const filledPdf = await pdfFillForm.write(pdfBuffer, formData, { save: 'pdf' });
+    Object.keys(formData).forEach(field => {
+      pdfDoc
+        .editPage(1) // Edit the first page
+        .text(formData[field], 'form.' + field) // Fill the form field
+        .endPage();
+    });
+
+    // End the PDF editing
+    pdfDoc.endPDF();
 
     // Send the filled PDF in the response
     console.log('Sending response');
     res.contentType('application/pdf');
-    res.send(filledPdf);
+    res.sendFile(outputPdfPath);
   } catch (err) {
     console.error('An error occurred:', err);
     res.status(500).send('An error occurred while processing the PDF.');
