ce2b7073f3ec1d877be9690d34472f9d997e2263
Sr-0w <139899120+Sr-0w@users.noreply.github.com>
2023-08-03 18:21:07 +0200

    GPT SAID IT

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const pdfFillForm = require('pdf-fill-form');
+const path = require('path'); // Add this line
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,20 +14,27 @@ app.get('/', (req, res) => {
 
 app.post('/submit', async (req, res) => {
   try {
+    console.log('Received POST request to /submit');
+
     // Use the form data to fill the PDF
     const formData = req.body;
+    console.log('Form data:', formData);
 
     // Read the PDF file
-    const pdfBuffer = fs.readFileSync('./public/securitashomeyoul.pdf');
+    console.log('Reading PDF file');
+    const pdfPath = path.join(__dirname, 'public', 'securitashomeyoul.pdf'); // Add this line
+    const pdfBuffer = fs.readFileSync(pdfPath); // Modify this line
 
     // Fill in the fields in the PDF
+    console.log('Filling PDF');
     const filledPdf = await pdfFillForm.write(pdfBuffer, formData, { save: 'pdf' });
 
     // Send the filled PDF in the response
+    console.log('Sending response');
     res.contentType('application/pdf');
     res.send(filledPdf);
   } catch (err) {
-    console.error(err);
+    console.error('An error occurred:', err);
     res.status(500).send('An error occurred while processing the PDF.');
   }
 });
