e1489bb3cde4d8eff9631301e3ddaa9635f308c3
Sr-0w <139899120+Sr-0w@users.noreply.github.com>
2023-08-03 17:23:31 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,49 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
-const pdffiller = require('pdffiller');
+const fs = require('fs');
+const pdfFillForm = require('pdf-fill-form');
 
 const app = express();
+app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public'))); // Serve static files from the "public" directory
+app.post('/pdf', async (req, res) => {
+  try {
+    // Read the readFields.txt file and parse the field names and values
+    const readFields = fs.readFileSync('public/readFields.txt', 'utf8');
+    const fields = readFields.split('\n').reduce((obj, line) => {
+      const [name, value] = line.split('=');
+      obj[name] = value;
+      return obj;
+    }, {});
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html')); // Send the HTML file for our form
+    // Read the PDF file
+    const pdfBuffer = fs.readFileSync('public/securitashomeyoul.pdf');
+
+    // Fill in the fields in the PDF
+    const filledPdf = await pdfFillForm.write(pdfBuffer, fields, { save: 'pdf' });
+
+    // Send the filled PDF in the response
+    res.contentType('application/pdf');
+    res.send(filledPdf);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('An error occurred while processing the PDF.');
+  }
 });
 
-app.post('/submit', (req, res) => {
-    console.log('Received POST request');
-
-    const sourcePDF = path.join(__dirname, 'securitashomeyoul.pdf');
-    const outputFilename = 'securitashomeyoul.pdffilled.pdf';
-    const outputPath = path.resolve(__dirname, 'public', outputFilename);
-
-    const data = {
-        Contract_Name: req.body.Contract_Name,
-        Contract_FirstName: req.body.Contract_FirstName,
-        Contract_Email: req.body.Contract_Email,
-        Contract_Phone_1: req.body.Contract_Phone_1,
-        Installation_Name: req.body.Installation_Name,
-        Contract_address: req.body.Contract_address,
-        Contract_PostalCode: req.body.Contract_PostalCode,
-        Contract_City: req.body.Contract_City,
-        Contract_Phone_2: req.body.Contract_Phone_2,
-        Installation_Email: req.body.Installation_Email
-    };
-
-    pdffiller.fillForm(sourcePDF, data, outputPath, function (err) {
-        if (err) {
-            console.error('Error occurred while filling the PDF form:', err);
-            // Respond with an error message or appropriate status code.
-            res.status(500).send('Error occurred while generating the PDF.');
-        } else {
-            console.log('Finished filling PDF form, sending filename');
-            // Respond with the file name or download link.
-            res.setHeader('Content-Disposition', `attachment; filename="${outputFilename}"`);
-            res.sendFile(outputPath);
-        }
-    });
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
-
-app.listen(3000, () => console.log('Server started on port 3000'));
