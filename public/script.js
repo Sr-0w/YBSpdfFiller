4d35119e445ab954265ae70203deca61d354bed0
Sr-0w <139899120+Sr-0w@users.noreply.github.com>
2023-08-04 20:59:55 +0200

    debug

--- public/script.js
+++ public/script.js
@@ -1,64 +1,62 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    document.getElementById('securitasForm').addEventListener('submit', function(event) {
-        event.preventDefault();
+document.getElementById('securitasForm').addEventListener('submit', function(event) {
+    event.preventDefault();
 
-        var formData = new FormData(event.target);
+    var formData = new FormData(event.target);
 
-        // If the "Same as client data" checkbox is checked, copy the client data to the installation data
-        if (document.getElementById('sameAsClientData').checked) {
-            formData.set('Installation_Name', formData.get('Contract_Name'));
-            formData.set('Installation_FirstName', formData.get('Contract_FirstName'));
-            formData.set('Installation_Email', formData.get('Contract_Email'));
-            formData.set('Installation_Street', formData.get('Contract_address'));
-            formData.set('Installation_StreetNr', formData.get('Contract_StreetNr'));
-            formData.set('Installation_PostalCode', formData.get('Contract_PostalCode'));
-            formData.set('Installation_City', formData.get('Contract_City'));
-            formData.set('Installation_Phone_1', formData.get('Contract_Phone_1'));
-            formData.set('Installation_Phone_2', formData.get('Contract_Phone_2'));
+    // If the "Same as client data" checkbox is checked, copy the client data to the installation data
+    if (document.getElementById('sameAsClientData').checked) {
+        formData.set('Installation_Name', formData.get('Contract_Name'));
+        formData.set('Installation_FirstName', formData.get('Contract_FirstName'));
+        formData.set('Installation_Email', formData.get('Contract_Email'));
+        formData.set('Installation_Street', formData.get('Contract_address'));
+        formData.set('Installation_StreetNr', formData.get('Contract_StreetNr'));
+        formData.set('Installation_PostalCode', formData.get('Contract_PostalCode'));
+        formData.set('Installation_City', formData.get('Contract_City'));
+        formData.set('Installation_Phone_1', formData.get('Contract_Phone_1'));
+        formData.set('Installation_Phone_2', formData.get('Contract_Phone_2'));
+    }
+
+    console.log('Sending POST request');
+    fetch('/submit', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(Object.fromEntries(formData)) // Convert formData to JSON
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
         }
+        // Get the filename from the response headers
+        const contentDisposition = response.headers.get('Content-Disposition');
+        const filename = contentDisposition.split('=')[1];
 
-        console.log('Sending POST request');
-        fetch('/submit', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(Object.fromEntries(formData)) // Convert formData to JSON
-          })
-          .then(response => {
-            if (!response.ok) {
-              throw new Error('Network response was not ok');
-            }
-            // Get the filename from the response headers
-            const contentDisposition = response.headers.get('Content-Disposition');
-            const filename = contentDisposition.split('=')[1];
+        return response.blob().then(blob => ({ blob, filename }));
+      })
+      .then(({ blob, filename }) => {
+        const url = URL.createObjectURL(blob); // Create a URL for the Blob
 
-            return response.blob().then(blob => ({ blob, filename }));
-          })
-          .then(({ blob, filename }) => {
-            const url = URL.createObjectURL(blob); // Create a URL for the Blob
+        // Create a link element
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = filename; // Set the download filename
 
-            // Create a link element
-            const link = document.createElement('a');
-            link.href = url;
-            link.download = filename; // Set the download filename
+        // Append the link to the body
+        document.body.appendChild(link);
 
-            // Append the link to the body
-            document.body.appendChild(link);
+        // Simulate a click on the link
+        link.click();
 
-            // Simulate a click on the link
-            link.click();
+        // Remove the link from the body
+        document.body.removeChild(link);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+});
 
-            // Remove the link from the body
-            document.body.removeChild(link);
-          })
-          .catch(error => {
-            console.error('Error:', error);
-          });
-    });
-
-    // Show or hide the installation data fields based on the "Same as client data" checkbox
-    document.getElementById('sameAsClientData').addEventListener('change', function(event) {
-        document.getElementById('installationData').style.display = event.target.checked ? 'none' : 'block';
-    });
+// Show or hide the installation data fields based on the "Same as client data" checkbox
+document.getElementById('sameAsClientData').addEventListener('change', function(event) {
+    document.getElementById('installationData').style.display = event.target.checked ? 'none' : 'block';
 });
