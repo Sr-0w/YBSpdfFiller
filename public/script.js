f07027f2d4fc4e25d7b8046398a283c755b80f8a
Sr-0w <139899120+Sr-0w@users.noreply.github.com>
2023-08-08 00:46:26 +0200

    cskl

--- public/script.js
+++ public/script.js
@@ -1,3 +1,25 @@
+// Define componentsMapping at a higher scope
+let componentsMapping = {};
+
+// Fetch component_mapping.json and use it in the script
+fetch('/path_to_component_mapping.json') // Adjust the path accordingly
+    .then(response => response.json())
+    .then(data => {
+        componentsMapping = data;
+
+        // Populate the components dropdown
+        const componentsDropdown = document.getElementById('componentsDropdown');
+        for (const component in componentsMapping) {
+            const option = document.createElement('option');
+            option.value = component;
+            option.textContent = component;
+            componentsDropdown.appendChild(option);
+        }
+    })
+    .catch(error => {
+        console.error('Error fetching component_mapping.json:', error);
+    });
+
 document.getElementById('securitasForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
@@ -85,16 +107,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
     });
 });
-
-// Populate the components dropdown
-const componentsDropdown = document.getElementById('componentsDropdown');
-const componentsData = {"Pack de base": "Aantal basispakket", "Incendie et CO (1 d\u00e9t. fum\u00e9e, 1 d\u00e9t. CO et 1 d\u00e9t chaleur)": "Aantal paquet Fire and CO", "Contact Pack (5 contacts)": "Aantal paquet contacts", "Smoke Pack (2 d\u00e9t fum\u00e9e)": "Aantal paquet smoke", "CAM Pack(1 cam\u00e9ra int\u00e9rieure et 1cam\u00e9ra ext\u00e9rieure)": "Aantal Pack Camera", "D\u00e9tecteur mouvement (PIR)": "Aantal Bewegingsdetector", "Contact magn\u00e9tique": "Aantal magneetcontacten", "D\u00e9tecteur d'eau": "Abonnement deurbel", "D\u00e9tecteur mouvement avec cam\u00e9ra (PIRcam)": "Aantal fotodetectoren", "D\u00e9tecteur de fum\u00e9e": "Aantal rookmelders", "D\u00e9tecteur de CO": "Aantal CO Detectors", "D\u00e9tecteur de chaleur": "Aantal heat Detectors", "IP cam\u00e9ra int\u00e9rieure": "Aantal binnencameras", "IP cam\u00e9ra ext\u00e9rieure": "Aantal buitencameras", "Cam\u00e9ra PoE dome": "Aantal POE DOME cameras", "Cam\u00e9ra PoE bullet": "Aantal POE BULLET cameras", "Sir\u00e8ne int\u00e9rieure": "Aantal Binnensirene", "Sir\u00e8ne ext\u00e9rieure": "Aantal Buitensirene", "Prise intelligente": "Aantal smartplug", "Sonnette intelligente": "Aantal deurbel abo", "Ampoule intelligente": "Aantal slimme lampen", "Coffre \u00e0 clefs": "Aantal sleutelkoffer", "D\u00e9tecteur bris de vitre acoustic": "Aantal Glassbreaks", "SVR 1T": "Aantal svr", "Adaptateur garage": "Aantal garage door adaptors", "Senseur temp\u00e9rature": "Aantal temperator sensors", "Contact magn\u00e9tique mince": "Aantal magnetic contact slim", "Serrure intelligente": "Aantal Smart Locks", "D\u00e9tecteur bris de vitre shock": "Aantal Glass and shock", "PIR ext\u00e9rieur": "Aantal buiten PIRCAM", "Switch 4 PoE": "Aantal POE Switch", "Clavier": "Aantal keypads", "Bouton panique": "Abonnement cam", "T\u00e9l\u00e9commande": "Aantal afstandsbediening", "Bracelet panique": "Aantal panic wrist", "Carrillon": "Aantal gong", "F1 amplificateur": "Aantal F1 Range Extender", "Pack animaux (6 contacts, 1 bris de vitre et 1 cam\u00e9ra int\u00e9rieure)": "Aantal Pack Pet", "Home Alone (5 contacts et 1 cam\u00e9ra ext\u00e9rieure)": "Aantal Pack Home Alone", "Ouverture de garage": "Aantal Garage Door Opener"}; // Components data from component_mapping.json
-for (const component in componentsData) {
-    const option = document.createElement('option');
-    option.value = component;
-    option.textContent = component;
-    componentsDropdown.appendChild(option);
-}
 
 // Handle the add component button
 const addComponentBtn = document.getElementById('addComponentBtn');
