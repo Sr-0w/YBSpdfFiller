11db9785fe0f0a4bd3f2b0419d8dd1c9f480023e
Sr-0w <139899120+Sr-0w@users.noreply.github.com>
2023-08-08 11:48:59 +0200

    d

--- readFields.js
+++ readFields.js
@@ -1,9 +1,14 @@
 const pdfFillForm = require('pdf-fill-form');
 const fs = require('fs');
 
+let fieldMapping = {};
+
 pdfFillForm.read('securitashomeyoul.pdf')
     .then(fields => {
-        console.log(fields);
+        fields.forEach(field => {
+            fieldMapping[field.id] = field.name;
+        });
+        fs.writeFileSync('fieldMapping.json', JSON.stringify(fieldMapping, null, 2));
     })
     .catch(err => {
         console.error(err);
