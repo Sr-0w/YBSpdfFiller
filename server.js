5fd90cd83c2d91e106854d9fba2a1e125f771ab7
Sr-0w <139899120+Sr-0w@users.noreply.github.com>
2023-08-08 19:52:02 +0200

    ok

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
-const pdfFillForm = require('pdf-fill-form');
+const { PDFDocument, PDFName } = require('pdf-lib');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,57 +10,56 @@ app.use(express.static('public'));
 
 let idToNameMapping = {};
 
-// Load the mapping between id and name when the server starts
-pdfFillForm.read(path.join(__dirname, 'public', 'securitashomeyoul.pdf'))
-    .then(fields => {
-        fields.forEach(field => {
-            idToNameMapping[field.id] = field.name;
-        });
-    })
-    .catch(err => {
-        console.error(err);
+const loadFieldMapping = async (pdfPath) => {
+    const pdfBytes = fs.readFileSync(pdfPath);
+    const pdfDoc = await PDFDocument.load(pdfBytes);
+    const form = pdfDoc.getForm();
+    const fields = form.getFields();
+    fields.forEach(field => {
+        idToNameMapping[field.getName()] = field.getName();
     });
+};
+
+loadFieldMapping(path.join(__dirname, 'public', 'securitashomeyoul.pdf'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-app.post('/submit', (req, res) => {
+app.post('/submit', async (req, res) => {
     try {
         console.log('Received POST request to /submit');
 
-        // Use the form data to fill the PDF
         const formData = req.body;
         console.log('Form data:', formData);
 
-        // Define the PDF file paths
         const pdfPath = path.join(__dirname, 'public', 'securitashomeyoul.pdf');
         const date = new Date();
         const formattedDate = `${date.getFullYear()}${date.getMonth() + 1}${date.getDate()}`;
-        const clientName = formData['131105'];
+        const clientName = formData['131105'];  // You might need to change this to match field names
         const uniqueId = Date.now();
         const outputPdfPath = path.join(__dirname, 'public', `${formattedDate} - ${clientName} - ${uniqueId}.pdf`);
 
         console.log('Filling PDF fields...');
-        let fillData = {};
-        for (let id in formData) {
-            const fieldName = idToNameMapping[id];
-            fillData[fieldName] = formData[id];
+        const pdfBytes = fs.readFileSync(pdfPath);
+        const pdfDoc = await PDFDocument.load(pdfBytes);
+        const form = pdfDoc.getForm();
+
+        for (let fieldName in formData) {
+            const field = form.getField(fieldName);
+            if (field) {
+                field.setValue(formData[fieldName]);
+            }
         }
+        form.flatten();
 
-        pdfFillForm.write(pdfPath, fillData, { "save": "pdf" })
-            .then(output => {
-                fs.writeFileSync(outputPdfPath, output);
-                console.log('Sending response with filled PDF');
-                res.contentType('application/pdf');
-                res.setHeader('Content-Disposition', `attachment; filename=${path.basename(outputPdfPath)}`);
-                res.sendFile(outputPdfPath);
-            })
-            .catch(err => {
-                console.error('An error occurred:', err);
-                res.status(500).send('An error occurred while processing the PDF.');
-            });
+        const filledPdfBytes = await pdfDoc.save();
+        fs.writeFileSync(outputPdfPath, filledPdfBytes);
 
+        console.log('Sending response with filled PDF');
+        res.contentType('application/pdf');
+        res.setHeader('Content-Disposition', `attachment; filename=${path.basename(outputPdfPath)}`);
+        res.sendFile(outputPdfPath);
     } catch (err) {
         console.error('An error occurred:', err);
         res.status(500).send('An error occurred while processing the PDF.');
